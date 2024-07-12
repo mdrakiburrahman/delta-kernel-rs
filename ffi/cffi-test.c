8c443bd8dfe70da470ca244f69dc60ffb8389df8
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-07-12 18:15:47 -0400

    Syntax

--- ffi/cffi-test.c
+++ ffi/cffi-test.c
@@ -88,8 +88,7 @@ int main(int argc, char* argv[]) {
 
   KernelStringSlice table_path_slice = { table_path, strlen(table_path) };
 
-  ExternResultHandleSharedExternEngine default_engine_res =
-    get_default_engine(table_path_slice, NULL);
+  ExternResultHandleSharedExternEngine default_engine_res = get_default_engine(table_path_slice, NULL);
   ExternResultHandleSharedExternEngine sync_engine_res = get_sync_engine(NULL);
 
   printf("Executing with default engine\n");
