aa9fc76863fbb5a5cfc8f10afea47f8d7324feff
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-07-20 19:40:16 -0400

    Factored TestEngine out and localtest

--- ffi-test-csharp/src/deltalake.kernel.rust.interop.ffi.test/Program.cs
+++ ffi-test-csharp/src/deltalake.kernel.rust.interop.ffi.test/Program.cs
@@ -1,30 +1,47 @@
-using Deltalake.Kernel.Rust.Interop.Ffi.Test.Callbacks.Delegates;
 using Deltalake.Kernel.Rust.Interop.Ffi.Test.Callbacks.Errors;
+using Deltalake.Kernel.Rust.Interop.Ffi.Test.Engines;
 using DeltaLake.Kernel.Rust.Ffi;
 using DeltaLake.Kernel.Rust.Interop.Ffi.Test.Extensions;
 using System.Runtime.InteropServices;
-using static Deltalake.Kernel.Rust.Interop.Ffi.Test.Delegates.VisitDelegates;
 
 namespace DeltaLake.Kernel.Rust.Interop.Ffi.Test
 {
-    public unsafe class FfiTestConsoleApp
+    public unsafe static class FfiTestConsoleApp
     {
         public static void Main(string[] args)
         {
             if (args.Length < 1)
             {
-                throw new ArgumentException("Usage: table/path");
+                throw new ArgumentException("Usage: 'localtable/path'");
             }
-            var tablePath = args[0];
+            var localTablePath = args[0];
+            bool isLocalTestPass = RunLocalTest(localTablePath);
 
-            Console.WriteLine($"Reading table at {tablePath}");
+            if (isLocalTestPass)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nTests succeeded :)\n");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nTest failed :(\n");
+                Console.ResetColor();
+                throw new InvalidOperationException("Test failed");
+            }
+        }
 
-            fixed (sbyte* tablePathPtr = tablePath.ToSByte())
+        private static bool RunLocalTest(string localTablePath)
+        {
+            Console.WriteLine($"Reading table at {localTablePath}");
+
+            fixed (sbyte* tablePathPtr = localTablePath.ToSByte())
             {
                 KernelStringSlice tablePathSlice = new KernelStringSlice
                 {
                     ptr = tablePathPtr,
-                    len = (nuint)tablePath.Length
+                    len = (nuint)localTablePath.Length
                 };
 
                 AllocateErrorFn callbackDelegate = AllocateErrorCallbacks.WarnAndThrowAllocateError;
@@ -36,109 +53,16 @@ namespace DeltaLake.Kernel.Rust.Interop.Ffi.Test
                     FFI_NativeMethodsHandler.get_sync_engine(callbackPointer);
 
                 Console.WriteLine($"Executing with default engine");
-                int defaultTestResult = TestEngine(tablePathSlice, defaultEngineRes);
+                int defaultTestResult = TestEngines.LocalTestEngine(
+                    tablePathSlice,
+                    defaultEngineRes
+                );
 
                 Console.WriteLine($"Executing with sync engine");
-                int syncTestResult = TestEngine(tablePathSlice, syncEngineRes);
+                int syncTestResult = TestEngines.LocalTestEngine(tablePathSlice, syncEngineRes);
 
-                if (defaultTestResult != 0 || syncTestResult != 0)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("\nTest failed :(\n");
-                    Console.ResetColor();
-                    throw new InvalidOperationException("Test failed");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("\nTests succeeded :)\n");
-                    Console.ResetColor();
-                }
+                return defaultTestResult == 0 && syncTestResult == 0;
             }
-        }
-
-        public static int TestEngine(
-            KernelStringSlice tablePathSlice,
-            ExternResultHandleSharedExternEngine engineRes
-        )
-        {
-            if (
-                engineRes.tag != ExternResultHandleSharedExternEngine_Tag.OkHandleSharedExternEngine
-            )
-            {
-                Console.WriteLine("Failed to get engine");
-                return -1;
-            }
-
-            SharedExternEngine* engine = engineRes.Anonymous.Anonymous1.ok;
-
-            ExternResultHandleSharedSnapshot snapshotRes = FFI_NativeMethodsHandler.snapshot(
-                tablePathSlice,
-                engine
-            );
-            if (snapshotRes.tag != ExternResultHandleSharedSnapshot_Tag.OkHandleSharedSnapshot)
-            {
-                Console.WriteLine("Failed to create snapshot");
-                return -1;
-            }
-
-            SharedSnapshot* snapshot = snapshotRes.Anonymous.Anonymous1.ok;
-
-            ulong v = FFI_NativeMethodsHandler.version(snapshot);
-            Console.WriteLine($"version: {v}");
-            ExternResultHandleSharedScan scanRes = FFI_NativeMethodsHandler.scan(
-                snapshot,
-                engine,
-                null
-            );
-            if (scanRes.tag != ExternResultHandleSharedScan_Tag.OkHandleSharedScan)
-            {
-                Console.WriteLine("Failed to create scan");
-                return -1;
-            }
-
-            SharedScan* scan = scanRes.Anonymous.Anonymous1.ok;
-
-            ExternResultHandleSharedScanDataIterator dataIterRes =
-                FFI_NativeMethodsHandler.kernel_scan_data_init(engine, scan);
-            if (
-                dataIterRes.tag
-                != ExternResultHandleSharedScanDataIterator_Tag.OkHandleSharedScanDataIterator
-            )
-            {
-                Console.WriteLine("Failed to construct scan data iterator");
-                return -1;
-            }
-
-            SharedScanDataIterator* dataIter = dataIterRes.Anonymous.Anonymous1.ok;
-
-            VisitDataDelegate callbackDelegate = VisitCallbacks.VisitData;
-            IntPtr callbackPointer = Marshal.GetFunctionPointerForDelegate(callbackDelegate);
-
-            // Iterate scanned files
-            //
-            for (; ; )
-            {
-                ExternResultbool okRes = FFI_NativeMethodsHandler.kernel_scan_data_next(
-                    dataIter,
-                    null,
-                    callbackPointer
-                );
-                if (okRes.tag != ExternResultbool_Tag.Okbool)
-                {
-                    Console.WriteLine("Failed to iterate scan data");
-                    return -1;
-                }
-                else if (!okRes.Anonymous.Anonymous1.ok)
-                {
-                    break;
-                }
-            }
-
-            FFI_NativeMethodsHandler.free_scan(scan);
-            FFI_NativeMethodsHandler.free_snapshot(snapshot);
-            FFI_NativeMethodsHandler.free_engine(engine);
-            return 0;
         }
     }
 }
